07acfb3486ab16cd5ddbe9ae5578cedc8629ac37
accounts15 <accounts15@kodego.ph>
2025-07-05 18:30:55 +0800

    Fix complaint bugs

--- resources/views/pages/customer_complaints/index.blade.php
+++ resources/views/pages/customer_complaints/index.blade.php
@@ -40,12 +40,9 @@
                                                 @endphp </td>
                                             <td> {{ date('F d, Y', strtotime($complaint['created_at'])) }} </td>
                                             <td>
-                                            <button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#complaintModal{{ $complaint['id'] }}">
-                                                <i class="fas fa-eye"></i> View
-                                            </button>
-
-                                            <!-- Modal -->
-                                            
+                                                <button type="button" class="btn btn-sm btn-primary" data-trigger="show-complaint-modal" data-complaint="{{ json_encode($complaint) }}">
+                                                    <i class="fas fa-eye"></i> View
+                                                </button>
                                             </td>
                                             
                                         </tr>
@@ -61,11 +58,11 @@
         </div>
     </div>
 
-    <div class="modal fade" id="complaintModal{{ $complaint['id'] }}" tabindex="-1" role="dialog" aria-labelledby="complaintModalLabel{{ $complaint['id'] }}" aria-hidden="true">
+    <div class="modal fade" id="complaintModal" tabindex="-1" role="dialog" aria-labelledby="complaintModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="complaintModalLabel{{ $complaint['id'] }}">
+                    <h5 class="modal-title" id="complaintModalLabel">
                         Complaint Details
                     </h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -76,20 +73,14 @@
                     <div class="row">
                         <div class="col-md-6">
                             <h6><strong>Customer Information:</strong></h6>
-                            <p><strong>Name:</strong> {{ ucwords($complaint['user']['lastname'] . ' ' . $complaint['user']['firstname'] . ' ' . $complaint['user']['middlename']) }}</p>
-                            <p><strong>Email:</strong> {{ $complaint['user']['email'] }}</p>
+                            <p><strong>Name:</strong> <font class="text-uppercase customer-name">{{ ucwords($complaint['user']['lastname'] . ' ' . $complaint['user']['firstname'] . ' ' . $complaint['user']['middlename']) }}</font></p>
+                            <p><strong>Email:</strong><font class="customer-email">{{ $complaint['user']['email'] }}</font></p>
                         </div>
                         <div class="col-md-6">
                             <h6><strong>Complaint Information:</strong></h6>
-                            <p><strong>Date:</strong> {{ date('F d, Y', strtotime($complaint['created_at'])) }}</p>
+                            <p><strong>Date:</strong> <font class="customer-date"></font></p>
                             <p><strong>Sentiment:</strong> 
-                                @if ($complaint['sentiments'] == 'positive')
-                                    <span class="badge badge-success">Positive</span>
-                                @elseif ($complaint['sentiments'] == 'negative')
-                                    <span class="badge badge-danger">Negative</span>
-                                @else
-                                    <span class="badge badge-warning">Neutral</span>
-                                @endif
+                                <font class="customer-sentiment"></font>
                             </p>
                         </div>
                     </div>
@@ -98,7 +89,7 @@
                         <div class="col-md-12">
                             <h6><strong>Complaint Description:</strong></h6>
                             <div class="border p-3 bg-light">
-                                {{ $complaint['complaint_description'] }}
+                                <font class="customer-complaint-description"></font>
                             </div>
                         </div>
                     </div>
@@ -112,3 +103,37 @@
 
 @include('partials.footer')
 <script src="{{ asset('assets/scripts/modules/scripts.js') }}"></script>
+
+<script>
+    $(document).ready(function() {
+        $('[data-trigger="show-complaint-modal"]').on('click', function(event) {
+            var button = $(this);
+            var complaint = JSON.parse($(this).attr('data-complaint'));
+            
+            let sentiment = '';
+            if (complaint['sentiments'] == 'positive') {
+                sentiment = '<span class="badge badge-success">Positive</span>';
+            } else if (complaint['sentiments'] == 'negative') {
+                sentiment = '<span class="badge badge-danger">Negative</span>';
+            } else {
+                sentiment = '<span class="badge badge-warning">Neutral</span>';
+            }
+            console.log(sentiment);
+            $('.customer-name').text(complaint['user']['lastname'] + ' ' + complaint['user']['firstname'] + ' ' + complaint['user']['middlename']);
+            $('.customer-email').text(complaint['user']['email']);
+            $('.customer-date').text(formatDate(complaint['created_at']));
+            $('.customer-sentiment').html(sentiment);
+            $('.customer-complaint-description').text(complaint['complaint_description']);
+
+            $('#complaintModal').modal('show');
+        });
+    });
+
+    function formatDate(created_at) {
+        let date = new Date(created_at);
+        let day = date.getDate();
+        let month = date.getMonth() + 1;
+        let year = date.getFullYear();
+        return `${day}/${month}/${year}`;
+    }
+</script>
